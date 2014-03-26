6ab79febb15e36738a30e31beff40962d7ee80eb
Marius Scheel <hi@mariusscheel.de>
2014-03-26 22:19:52 +0100

    remove gitkeep, update instapaper description

--- js/instapaper.com.js
+++ js/instapaper.com.js
@@ -1,6 +1,6 @@
 /* 
  * the idea of using the first part of the email adress as 
- * username is nice, but totally annoying when you've an mail
- * like hi@example.org. So let's change that!
+ * username is nice, but totally annoying when you've got an mail
+ * adress like hi@example.org. So let's change that!
  */
 document.getElementById('user_row').innerHTML = 'Your Account <i class="icon-cog"></i>';
