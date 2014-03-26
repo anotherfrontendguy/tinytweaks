e045929b7df5cd3e66f11f11d4557b7410f57356
Marius Scheel <hi@mariusscheel.de>
2014-03-26 22:17:34 +0100

    add instapaper js to change wording on acc button

---
+++ js/instapaper.com.js
@@ -0,0 +1,6 @@
+/* 
+ * the idea of using the first part of the email adress as 
+ * username is nice, but totally annoying when you've an mail
+ * like hi@example.org. So let's change that!
+ */
+document.getElementById('user_row').innerHTML = 'Your Account <i class="icon-cog"></i>';
